25d7b8bc1805b4f5b59d14ad3fc9ba1310276e15
mot <motoharu0127k@gmail.com>
2025-06-24 14:36:21 +0900

    新着作品ページの作成と、遷移用のnavbarの修正

--- dokkai-craft/src/app/new/page.tsx
+++ dokkai-craft/src/app/new/page.tsx
@@ -4,7 +4,7 @@ import React, { useState } from "react"
 import Link from "next/link"
 import Image from "next/image"
 
-// アイコン
+// アイコンa
 const HeartIcon = () => (
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none"
          stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
